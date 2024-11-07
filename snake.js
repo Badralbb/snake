d86c41e342a6a357338e8f80cec8fd2c14e73113
badral <badral@badrals-MacBook-Pro.local>
2024-11-07 23:21:52 +0800

    last commit

--- snake.js
+++ snake.js
@@ -11,180 +11,179 @@ const foodat = document.getElementById("food");
 let direction = "right";
 let speed = 100;
 let reverseSpeed = 5;
-const backbutton = () =>{
-    body.style.display = "none";
-    area.style.display = "none";
-    document.getElementById("basic").style.display = "block";
-    document.getElementById("basic").style.display = "flex";
-}
+const backbutton = () => {
+  body.style.display = "none";
+  area.style.display = "none";
+  document.getElementById("basic").style.display = "block";
+  document.getElementById("basic").style.display = "flex";
+};
 const start = () => {
-    body.style.display = "block";
-    area.style.display = "block";
-    document.getElementById("basic").style.display = "none";
-    setInterval(gameloop, speed - reverseSpeed);
+  body.style.display = "block";
+  area.style.display = "block";
+  document.getElementById("basic").style.display = "none";
+  setInterval(gameloop, speed - reverseSpeed);
 };
 const lvl1 = () => {
-    reverseSpeed = 7;
+  reverseSpeed = 7;
 
-    start();
-
+  start();
 };
 const lvl2 = () => {
-    reverseSpeed = 10;
-    setInterval(gameloop, speed - reverseSpeed);
-    start();
+  reverseSpeed = 10;
+  setInterval(gameloop, speed - reverseSpeed);
+  start();
 };
 const lvl3 = () => {
-    reverseSpeed = 15;
-    setInterval(gameloop, speed - reverseSpeed);
-    start();
+  reverseSpeed = 15;
+  setInterval(gameloop, speed - reverseSpeed);
+  start();
 };
 let bodyCoordinates = [
-    { x: 5, y: 5 },
-    { x: 6, y: 5 },
-    { x: 7, y: 5 },
-    { x: 8, y: 5 },
+  { x: 5, y: 5 },
+  { x: 6, y: 5 },
+  { x: 7, y: 5 },
+  { x: 8, y: 5 },
 ];
 const food = {
-    x: 5,
-    y: 5,
+  x: 5,
+  y: 5,
 };
 const goRight = () => {
-    positionX += 1;
-    if (positionX > areaWidth - 1) {
-        positionX = 0;
-    }
+  positionX += 1;
+  if (positionX > areaWidth - 1) {
+    positionX = 0;
+  }
 };
 const eatFood = () => {
-    render();
+  render();
 };
 const generateFood = () => {
-    food.x = Math.floor(Math.random() * scale);
-    food.y = Math.floor(Math.random() * scale);
-    foodat.style.left = `${food.x * scale}px`;
-    foodat.style.top = `${food.y * scale}px`;
-}
+  food.x = Math.floor(Math.random() * scale);
+  food.y = Math.floor(Math.random() * scale);
+  foodat.style.left = `${food.x * scale}px`;
+  foodat.style.top = `${food.y * scale}px`;
+};
 const render = () => {
-    let bodyHtml = "";
+  let bodyHtml = "";
 
-    if (food.x == positionX && food.y == positionY) {
-        bodyCoordinates.unshift(bodyCoordinates[0]);
-        point += 5;
-        generateFood();
-    
-        document.getElementById("score").innerText = point;
-    }
+  if (food.x == positionX && food.y == positionY) {
+    bodyCoordinates.unshift(bodyCoordinates[0]);
+    point += 5;
+    generateFood();
 
-    for (let i = 0; i < bodyCoordinates.length; i++) {
-        bodyHtml += `<div class="snake-core" style="top:${bodyCoordinates[i].y * scale
-            }px;left:${bodyCoordinates[i].x * scale}px"></div>`;
-    }
-    body.innerHTML = bodyHtml;
+    document.getElementById("score").innerText = point;
+  }
 
-    foodat.style.left = `${food.x * scale}px`;
-    foodat.style.top = `${food.y * scale}px`;
+  for (let i = 0; i < bodyCoordinates.length; i++) {
+    bodyHtml += `<div class="snake-core" style="top:${
+      bodyCoordinates[i].y * scale
+    }px;left:${bodyCoordinates[i].x * scale}px"></div>`;
+  }
+  body.innerHTML = bodyHtml;
+
+  foodat.style.left = `${food.x * scale}px`;
+  foodat.style.top = `${food.y * scale}px`;
 };
 
 const goLeft = () => {
-    positionX -= 1;
-    if (positionX < 0) {
-        positionX = areaWidth - 1;
-    }
+  positionX -= 1;
+  if (positionX < 0) {
+    positionX = areaWidth - 1;
+  }
 };
 const goUp = () => {
-    positionY -= 1;
-    if (positionY < 0) {
-        positionY = areaHeight - 1;
-    }
+  positionY -= 1;
+  if (positionY < 0) {
+    positionY = areaHeight - 1;
+  }
 };
 const goDown = () => {
-    positionY += 1;
-    if (positionY > areaHeight - 1) {
-        positionY = 0;
-    }
+  positionY += 1;
+  if (positionY > areaHeight - 1) {
+    positionY = 0;
+  }
 };
 
 const resetGame = () => {
-    area.style.width = `${areaWidth * scale}px`;
-    area.style.height = `${areaHeight * scale}px`;
+  area.style.width = `${areaWidth * scale}px`;
+  area.style.height = `${areaHeight * scale}px`;
 
-
-
-    generateFood();
-
+  generateFood();
 };
 const changeDirection = (value) => {
-    if (direction == "up" || direction == "down") {
-        if (value == "left" || value == "right") {
-            direction = value;
-        }
-    } else if (direction == "right" || direction == "left") {
-        if (value == "up" || value == "down") {
-            direction = value;
-        }
+  if (direction == "up" || direction == "down") {
+    if (value == "left" || value == "right") {
+      direction = value;
     }
+  } else if (direction == "right" || direction == "left") {
+    if (value == "up" || value == "down") {
+      direction = value;
+    }
+  }
 };
 const handleKeyDown = (event) => {
-    switch (event.key) {
-        case "w":
-        case "ArrowUp":
-            changeDirection("up");
-            break;
-        case "a":
-        case "ArrowLeft":
-            changeDirection("left");
-            break;
-        case "s":
-        case "ArrowDown":
-            changeDirection("down");
-            break;
-        case "d":
-        case "ArrowRight":
-            changeDirection("right");
-            break;
-    }
+  switch (event.key) {
+    case "w":
+    case "ArrowUp":
+      changeDirection("up");
+      break;
+    case "a":
+    case "ArrowLeft":
+      changeDirection("left");
+      break;
+    case "s":
+    case "ArrowDown":
+      changeDirection("down");
+      break;
+    case "d":
+    case "ArrowRight":
+      changeDirection("right");
+      break;
+  }
 };
 const gameloop = () => {
-    switch (direction) {
-        case "up":
-            goUp();
-            break;
-        case "down":
-            goDown();
-            break;
-        case "right":
-            goRight();
-            break;
-        case "left":
-            goLeft();
-            break;
-    }
-    gameOver();
+  switch (direction) {
+    case "up":
+      goUp();
+      break;
+    case "down":
+      goDown();
+      break;
+    case "right":
+      goRight();
+      break;
+    case "left":
+      goLeft();
+      break;
+  }
+  gameOver();
 
-    bodyCoordinates.shift();
-    bodyCoordinates.push({ x: positionX, y: positionY });
+  bodyCoordinates.shift();
+  bodyCoordinates.push({ x: positionX, y: positionY });
 
-    render();
+  render();
 };
 
 const gameOver = () => {
-    for (let i = 0; i < bodyCoordinates.length; i++) {
-        if (positionX == bodyCoordinates[i].x && positionY == bodyCoordinates[i].y) {
-            alert("Game Over");
-            bodyCoordinates = [
-                { x: 5, y: 5 },
-                { x: 6, y: 5 },
-                { x: 7, y: 5 },
-                { x: 8, y: 5 },
-            ];
-            positionX = 6;
-            positionY = 6;
-            point = 0;
-            document.getElementById("score").innerText = point
-            break;
-        }
-
+  for (let i = 0; i < bodyCoordinates.length; i++) {
+    if (
+      positionX == bodyCoordinates[i].x &&
+      positionY == bodyCoordinates[i].y
+    ) {
+      bodyCoordinates = [
+        { x: 5, y: 5 },
+        { x: 6, y: 5 },
+        { x: 7, y: 5 },
+        { x: 8, y: 5 },
+      ];
+      positionX = 6;
+      positionY = 6;
+      point = 0;
+      document.getElementById("score").innerText = point;
+      alert("Game Over");
+      break;
     }
-}
+  }
+};
 
 resetGame();
